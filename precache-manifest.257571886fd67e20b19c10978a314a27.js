e5a2ebf5ddf2ecc4c0f1b69aeb7d3f1bc464f537
Poojan <ppatel2099@gmail.com>
2022-06-03 08:56:04 -0700

    Updates

---
+++ precache-manifest.257571886fd67e20b19c10978a314a27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac636b10bb2e3a2f62355afe572c98d6",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "f83ee99a03e16ca99bf8",
+    "url": "/Pokemon/static/css/main.55ef3c11.chunk.css"
+  },
+  {
+    "revision": "ca371f781afca374f62b",
+    "url": "/Pokemon/static/js/2.0008ad52.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/Pokemon/static/js/2.0008ad52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83ee99a03e16ca99bf8",
+    "url": "/Pokemon/static/js/main.9799dafa.chunk.js"
+  },
+  {
+    "revision": "ddf05dce3ab00bdaf8bc",
+    "url": "/Pokemon/static/js/runtime-main.b9375719.js"
+  }
+]);
